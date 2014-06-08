4a38264b682ca677af20209fb955ecf9cf105328
HailangGao <gaohailang@wandoujia.com>
2014-06-08 20:49:35 +0800

    feat: add transfer folder, add a test scaffold

---
+++ test/spec.js
@@ -0,0 +1,17 @@
+describe('Test Transfer Layer', function() {
+    it('should check json syntax', {
+
+    });
+
+    it('should read apiconfig.json, and add a new item, and save', function() {
+
+    });
+
+    it('should reload the server', function() {
+        // after config update, reload express server
+    });
+
+    it('should accept a resource json, put it in data directory', function() {
+
+    });
+});
