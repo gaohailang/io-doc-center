c996633920ae346b7019316815394de20b5167ee
HailangGao <gaohailang@wandoujia.com>
2014-06-09 11:59:50 +0800

    feat: add gs-helper scaffold

---
+++ helper-gs/main.js
@@ -0,0 +1,34 @@
+/**
+ * Creates a menu entry in the Google Docs UI when the document is opened.
+ *
+ * @param {object} e The event parameter for a simple onOpen trigger. To
+ *     determine which authorization mode (ScriptApp.AuthMode) the trigger is
+ *     running in, inspect e.authMode.
+ */
+function onOpen(e) {
+    DocumentApp.getUi().createAddonMenu()
+        .addItem('Start', 'showSidebar')
+        .addToUi();
+}
+
+/**
+ * Runs when the add-on is installed.
+ *
+ * @param {object} e The event parameter for a simple onInstall trigger. To
+ *     determine which authorization mode (ScriptApp.AuthMode) the trigger is
+ *     running in, inspect e.authMode. (In practice, onInstall triggers always
+ *     run in AuthMode.FULL, but onOpen triggers may be AuthMode.LIMITED or
+ *     AuthMode.NONE.)
+ */
+function onInstall(e) {
+    onOpen(e);
+}
+
+/**
+ * Opens a sidebar in the document containing the add-on's user interface.
+ */
+function showSidebar() {
+    var ui = HtmlService.createHtmlOutputFromFile('sidebar.html')
+        .setTitle('wdj API Helper');
+    DocumentApp.getUi().showSidebar(ui);
+}
